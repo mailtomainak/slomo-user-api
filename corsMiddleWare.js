57fc3deff335fb2aea92b655460d0cb899c513e6
mailtomainak <mailtomainak@gmail.com>
2018-12-21 19:02:32 +0530

    Enable Options

--- corsMiddleWare.js
+++ corsMiddleWare.js
@@ -3,7 +3,14 @@ const enableCors = (req,res,next)=>{
     //temporarily  allowed . TODO change Origin
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "x-slomo-auth");
-    next();
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    if(req.method==='OPTIONS'){
+        res.send(200);
+    }
+    else{
+        next();
+    }
 }
 
 module.exports = enableCors;
+
