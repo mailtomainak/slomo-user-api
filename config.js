42fd1898840b53dac3918518023cfc45e85e9c74
mailtomainak <mailtomainak@gmail.com>
2018-12-21 21:47:53 +0530

    fix incorrect config key

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports  = {
     PORT:process.env.PORT || 8082,
     TOKEN_SECRET:process.env.TOKEN_SECRET || '-write-yr-secret',
-    MONGO_URL:process.env.MONGODB_URI || "-local-mongo",
+    MONGO_URL:process.env.MONGO_URL || "-local-mongo",
     AUTH_TOKEN_NAME:'x-slomo-auth'
 }
