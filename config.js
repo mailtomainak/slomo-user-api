d5b3a8a615c97b7526bf2bca6b978109691fb0b5
mailtomainak <mailtomainak@gmail.com>
2019-05-02 12:13:36 +0200

    edit config file

--- config.js
+++ config.js
@@ -2,7 +2,8 @@ module.exports  = {
     PORT:process.env.PORT || 8082,
     TOKEN_SECRET:process.env.TOKEN_SECRET || '-write-yr-secret',
     MONGO_URL:process.env.MONGO_URL || "-local-mongo",
-    LOGGLY_SUBDOMAIN:process.env.LOGGLY_SUBDOMAIN || "-loggly subdomain",
-    LOGGLY_TOKEN:process.env.LOGGLY_TOKEN || "-loggly-token",
+    LOGGLY_SUBDOMAIN:process.env.LOGGLY_SUBDOMAIN || "--subdomain",
+    LOGGLY_TOKEN:process.env.LOGGLY_TOKEN || "--loggly-token",
     AUTH_TOKEN_NAME:'x-slomo-auth'
 }
+
